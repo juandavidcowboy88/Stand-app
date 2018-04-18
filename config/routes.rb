3526a02f10c028e2796092eb40a1c76cf008e481
juandavid.alvarez <ing.juandavid.alvarez@gmail.com>
2018-04-17 23:35:43 -0500

    routes after sign_in_path_for to index#artists = /artists

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+  root 'general#index'
   devise_for :artists
   resources :events
-  resources :artists, controllers: { registrations: "registrations" }
-  root 'general#index'
-    authenticated :artists do
-         root to: 'artists#index', as: :authenticated_root
+  resources :artists
+  get '/artists' => 'artists#index', as: :artist_root #create artists_root
+  namespace :artist do
+   root 'artists#index' # creates user_root_path
    end
+
+  
+  authenticated :artists do
+   root to: 'artists#index', as: :authenticated_root
+  end
+
+
+
+
+
    get 'new_post_modal' => 'artist#new_artist_modal'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
