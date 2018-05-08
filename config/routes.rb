3bbaa981a7a3cf69e87d0db963a43b5202c121b7
juandavid.alvarez <ing.juandavid.alvarez@gmail.com>
2018-05-08 12:30:24 -0500

    routes added the nested route artist to event

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'general#index'
   devise_for :artists
-  resources :events
+  resources :events do
+    resources 'artists', only: [:index,:new, :edit]
+  end
   resources :artists do
-      resources 'events', only: [:index,:new]
+      resources 'events', only: [:index,:new, :edit]
   end
 
   get '/artists' => 'artists#index', as: :artist_root #create artists_root
