a4c8d55803386a7951e28930d5a3f8ce0dbefcab
juandavid.alvarez <ing.juandavid.alvarez@gmail.com>
2018-05-08 12:20:12 -0500

    routes added the nested route event to artist

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ApplicationRecord
-  has_and_belongs_to_many  :artist
-
+  has_and_belongs_to_many  :artists
 
   def start_time
         self.date_event ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,20 @@ Rails.application.routes.draw do
   root 'general#index'
   devise_for :artists
   resources :events
-  resources :artists
+  resources :artists do
+      resources 'events', only: [:index,:new]
+  end
+
   get '/artists' => 'artists#index', as: :artist_root #create artists_root
   namespace :artist do
    root 'artists#index' # creates user_root_path
    end
-
-  
-  authenticated :artists do
+   authenticated :artists do
    root to: 'artists#index', as: :authenticated_root
-  end
+    end
 
 
-
-
-
-   get 'new_post_modal' => 'artist#new_artist_modal'
+    get 'new_post_modal' => 'artist#new_artist_modal'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
